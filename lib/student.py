632594fc2c39b6606cd1eb87426fc7427add4141
cheruiyotrop <cheruiyot.rop@student.moringaschool.com>
2024-03-05 23:37:08 +0300

    1st commit

--- lib/student.py
+++ lib/student.py
@@ -1,5 +1,27 @@
-class Student:
-    pass
+# student.py
+from user import User
+
+class Student(User):
+    def __init__(self, first_name, last_name):
+        super().__init__(first_name, last_name)
+        self.knowledge = []
+
+    def learn(self, new_knowledge):
+        self.knowledge.append(new_knowledge)
+
+    def hello(self):
+        print(f"Hello, I'm {self.first_name} {self.last_name}. I'm excited to learn!")
+
+    def raise_hand(self):
+        print("Pick me!")
+
+from student import Student
 
 class ChattyStudent(Student):
-    pass
+    def hello(self):
+        super().hello()
+        print("How are you doing today? I'm okay, but I'm kind of tired. Did you watch The Walking Dead last night? You didn't?! Oh man, it was so crazy! What, you don't want any spoilers? Okay well let me just tell you who died...")
+
+    def raise_hand(self):
+        for _ in range(10):
+            super().raise_hand()
